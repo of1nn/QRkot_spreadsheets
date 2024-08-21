de43a3b890b156613c2cfb3365ffc1ad2a9d3a7a
Ofinn <dmitrijzajc175@gmail.com>
2024-08-21 10:19:55 +0300

    fix flake8 n1

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -2,7 +2,6 @@ from aiogoogle import Aiogoogle
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas import CharityProjectDB
 from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.core.user import current_superuser
